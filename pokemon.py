21aa4d7ec08e46042d450d33f10f99a658643d13
Marcos Aguilar <marcosaguilar2001mal@gmail.com>
2024-12-04 16:57:56 +0100

    initial commit

---
+++ pokemon.py
@@ -0,0 +1,15 @@
+import random
+
+pikachu_heald = 80
+squirtle_heald = 90
+
+bola_voltio = 10
+onda_trueno = 11
+
+placaje = 10
+pistola_agua = 12
+burbuja = 9
+
+
+while pikachu_heald > 0 and squirtle_heald > 0:
+
